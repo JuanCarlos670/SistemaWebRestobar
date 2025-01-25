a638ee1048c211c51614fda8c48a28194c6f1538
Ericson-1412 <caldito699@gmail.com>
2025-01-25 10:15:04 -0500

    conexionDB

--- src/Proyecto.js
+++ src/Proyecto.js
@@ -2,7 +2,7 @@ import express from 'express';
 import {dirname,join} from 'path';
 import {fileURLToPath} from 'url';
 import router from './routes/index.js';
-
+import { Conectar } from './public/services/Conexion.js';
 const app = express();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -13,5 +13,8 @@ app.use(express.static(join(__dirname, 'public')));
 app.use(router);
 
 // app.get('/', (req, res) => res.render('index'));
-app.listen(3000);
-console.log('Server is running on http://localhost:3000');
+// Conectar a la base de datos
+Conectar();
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
