4f4e14199b3e0a2e9cee886349a3331a88a6ad28
Ericson-1412 <caldito699@gmail.com>
2025-01-28 16:57:55 -0500

    Conexion principal

--- src/Proyecto.js
+++ src/Proyecto.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {dirname,join} from 'path';
 import {fileURLToPath} from 'url';
 import router from './routes/index.js';
+import { Conectar } from './public/services/Conexion.js';
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.static(join(__dirname, 'public')));
 
 app.use(router);
 
+Conectar();
 // app.get('/', (req, res) => res.render('index'));
 app.listen(3000);
 console.log('Server is running on http://localhost:3000');
---
+++ src/public/services/Conexion.js
@@ -0,0 +1,22 @@
+import pg from 'pg';
+const { Client } = pg;
+const config ={
+    user: 'db_restobar_user',
+    host: 'dpg-cuaenvaj1k6c73e4rqfg-a.oregon-postgres.render.com',
+    database: 'db_restobar',
+    password: 'vkpL6RifKKNVOWqjJi2bAOWHJJpnny1D',
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+export async function Conectar() {
+    const cliente = new Client(config);
+    try {
+        await cliente.connect();
+        console.log('Conexión exitosa');
+        return cliente;
+    } catch (error) {
+        console.error('Error al conectar a la base de datos:', error);
+    }
+}
